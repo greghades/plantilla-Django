78aacf09b9062e6323d059ed6f15f02011321bb5
gregjose25131 <gregjose2513@gmail.com>
2022-12-28 22:42:35 -0400

    first Commit

---
+++ pruebaEntornos/settings/local.py
@@ -0,0 +1,32 @@
+from .base import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-nhipm18hs$fm*+rq@bg&a((e#2^f0r1#a((yh9v&xtkpywsa@m'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS  = [
+    BASE_DIR / 'static',
+]
